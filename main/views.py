6c96767c5bc521e1bbcd90d5821ab0949530ea1b
youtune <youtune8000@gmail.com>
2021-03-06 20:32:21 -0600

    first commit

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 def home(request):
     return render(request, 'main/home.htm')
 
 def updates(request):
-    return render(request, 'main/updates.htm')
+    projects = Project.objects.all()
+    return render(request, 'main/updates.htm', {'projects':projects})
 # Create your views here.
